70b5e45ef7ec83e1a3b3ec4a7da8d9ad2969fddf
= <davidelitemantra@gmail.com>
2021-04-21 14:37:10 +0530

    update product

--- src/app/products/delete-product/delete-product.component.ts
+++ src/app/products/delete-product/delete-product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-delete-product',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delete-product.component.scss']
 })
 export class DeleteProductComponent implements OnInit {
+  productId = 0;
 
-  constructor() { }
+  constructor(private activatedRoute :ActivatedRoute,
+    private productServie : ProductService) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(data =>{
+      this.productId = data.id; //capture the ID which i want deleted product
+
+      this.productServie.deleteProduct(this.productId).subscribe(deleteProduct => {
+        console.log("Product has been Delete"); //deleted Data selected id 
+      })
+    })
   }
 
 }
--- src/app/products/update-product/update-product.component.ts
+++ src/app/products/update-product/update-product.component.ts
@@ -1,5 +1,7 @@
+import { formatCurrency } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-update-product',
@@ -7,13 +9,38 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./update-product.component.scss']
 })
 export class UpdateProductComponent implements OnInit {
-productID = 0;
-  constructor( private activatedRoute: ActivatedRoute) { }
+productId = 0;
+  productDetails: import("d:/shopping/shopping/src/app/products/product").Product;
+  constructor( private activatedRoute: ActivatedRoute,
+    private productService: ProductService) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(data=>{
-      this.productID = data.id;
-    })
+      this.productId = data.id;
+
+      this.productService.viewProduct(this.productId).subscribe(productData => {
+        this.productDetails = productData;
+        console.log(this.productDetails);
+      });
+    });
   }
+  updateProduct(form: any) {
+    const updateProduct = {
+     id : form.value.id,
+     productName: form.value.product_name,
+     categoryid : form.value.product_category,
+     descriptions: form.value.product_description,
+     rating: form.value.product_rating,
+     color: form.value.product_color,
+     isAvailable:1,
+     productImg: '',
+     price: form.value.product_price,
+     review: form.value.product_reviews
+   }
+   console.log(form);
+   this.productService.updateProduct(this.productId, updateProduct ).subscribe(data =>{
+     console.log(data);
+   })
+ }
 
 }
